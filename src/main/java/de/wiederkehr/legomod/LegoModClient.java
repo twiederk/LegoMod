e66e90c2554ca42e59f48e3ec24f36b247c4473d
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2026-06-28 17:47:35 +0200

    Clean up code

--- src/main/java/de/wiederkehr/legomod/LegoModClient.java
+++ src/main/java/de/wiederkehr/legomod/LegoModClient.java
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = LegoMod.MODID, dist = Dist.CLIENT)
+@Mod(value = LegoMod.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = LegoMod.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = LegoMod.MOD_ID, value = Dist.CLIENT)
 public class LegoModClient {
     public LegoModClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
