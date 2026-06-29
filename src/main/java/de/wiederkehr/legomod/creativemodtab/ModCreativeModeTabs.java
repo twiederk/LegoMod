1c7b978e1b73a73136886fce281a5599490b5e1d
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2026-06-29 06:15:51 +0200

    Add Azurite items tab positioning in creative mode

--- src/main/java/de/wiederkehr/legomod/creativemodtab/ModCreativeModeTabs.java
+++ src/main/java/de/wiederkehr/legomod/creativemodtab/ModCreativeModeTabs.java
@@ -4,7 +4,9 @@ import de.wiederkehr.legomod.LegoMod;
 import de.wiederkehr.legomod.item.ModItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -20,6 +22,8 @@ public class ModCreativeModeTabs {
             () -> CreativeModeTab.builder()
                     .icon(() -> new ItemStack(ModItems.AZURITE.get()))
                     .title(Component.translatable("creativetab.legomod.azurite_items"))
+                    .withTabsBefore(CreativeModeTabs.INGREDIENTS)
+                    .withTabsAfter(Identifier.fromNamespaceAndPath(LegoMod.MOD_ID, "azurite_blocks_tab"))
                     .displayItems((itemDisplayParameters, output) -> {
                         output.accept(ModItems.AZURITE.get());
                         output.accept(ModItems.RAW_AZURITE.get());
