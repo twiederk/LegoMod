1cdc1795206c79dc8a69806a293bf59e33d2f5e6
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2026-06-29 19:21:02 +0200

    Added Azurite block

--- src/main/java/de/wiederkehr/legomod/block/ModBlocks.java
+++ src/main/java/de/wiederkehr/legomod/block/ModBlocks.java
@@ -1,12 +1,37 @@
 package de.wiederkehr.legomod.block;
 
 import de.wiederkehr.legomod.LegoMod;
+import de.wiederkehr.legomod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Function;
 
 public class ModBlocks {
 
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(LegoMod.MOD_ID);
+
+    public static final DeferredBlock<Block> AZURITE_BLOCK = registerBlock("azurite_block",
+            properties -> new Block(properties
+                    .strength(4f)
+                    .requiresCorrectToolForDrops()
+                    .sound(SoundType.AMETHYST)
+            ));
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Function<BlockBehaviour.Properties, T> function) {
+        DeferredBlock<T> toReturn = BLOCKS.registerBlock(name, function);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.registerItem(name, properties -> new BlockItem(block.get(), properties.useBlockDescriptionPrefix()));
+    }
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
