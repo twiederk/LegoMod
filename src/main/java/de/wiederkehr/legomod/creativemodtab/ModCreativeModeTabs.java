4f85f0a5c32125f17e63cf48469734563685cf75
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2026-06-29 06:10:07 +0200

    Add Azurite items creative mode tab and localization entries

--- src/main/java/de/wiederkehr/legomod/creativemodtab/ModCreativeModeTabs.java
+++ src/main/java/de/wiederkehr/legomod/creativemodtab/ModCreativeModeTabs.java
@@ -1,15 +1,30 @@
 package de.wiederkehr.legomod.creativemodtab;
 
 import de.wiederkehr.legomod.LegoMod;
+import de.wiederkehr.legomod.item.ModItems;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
 
 public class ModCreativeModeTabs {
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LegoMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> AZURITE_ITEMS_TAB = CREATIVE_MODE_TABS.register("azurite_items_tab",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(ModItems.AZURITE.get()))
+                    .title(Component.translatable("creativetab.legomod.azurite_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.AZURITE.get());
+                        output.accept(ModItems.RAW_AZURITE.get());
+                    })
+                    .build());
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
