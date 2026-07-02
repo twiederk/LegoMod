dcd248bec321b84d34e200314c5b75f88fd47866
IN57KC <torsten.wiederkehr@ing.com>
2026-07-02 18:26:59 +0200

    multi drop method

--- src/main/java/de/wiederkehr/legomod/datagen/ModBlockLootTableProvider.java
+++ src/main/java/de/wiederkehr/legomod/datagen/ModBlockLootTableProvider.java
@@ -1,11 +1,21 @@
 package de.wiederkehr.legomod.datagen;
 
 import de.wiederkehr.legomod.block.ModBlocks;
+import de.wiederkehr.legomod.item.ModItems;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 
 import java.util.Set;
 
@@ -17,8 +27,25 @@ public class ModBlockLootTableProvider extends BlockLootSubProvider {
 
     @Override
     protected void generate() {
+        dropSelf(ModBlocks.AZURITE_BLOCK.get());
+        // dropSelf(ModBlocks.RAW_AZURITE_BLOCK.get());
 
+        add(ModBlocks.AZURITE_ORE.get(),
+                createOreDrop(ModBlocks.AZURITE_ORE.get(), ModItems.RAW_AZURITE.get()));
+        add(ModBlocks.AZURITE_DEEPSLATE_ORE.get(),
+                createOreDrop(ModBlocks.AZURITE_DEEPSLATE_ORE.get(), ModItems.RAW_AZURITE.get()));
     }
+
+    protected LootTable.Builder createMultipleOreDrops(Block block, Item item, float min_drops, float max_drops) {
+        HolderLookup.RegistryLookup<Enchantment> enchantments = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(block, this.applyExplosionDecay(block,
+                        LootItem.lootTableItem(item)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(min_drops, max_drops)))
+                                .apply(ApplyBonusCount.addOreBonusCount(enchantments.getOrThrow(Enchantments.FORTUNE)))
+                )
+        );
+    }
+
 
     @Override
     protected Iterable<Block> getKnownBlocks() {
