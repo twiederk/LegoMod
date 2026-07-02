e3f8fce99c5e473b1d5b45a5db07e8316a066a88
IN57KC <torsten.wiederkehr@ing.com>
2026-07-02 18:06:28 +0200

    Started with ModBlockLootTableProvider implementation

--- src/main/java/de/wiederkehr/legomod/datagen/ModBlockLootTableProvider.java
+++ src/main/java/de/wiederkehr/legomod/datagen/ModBlockLootTableProvider.java
@@ -1,4 +1,27 @@
 package de.wiederkehr.legomod.datagen;
 
-public class ModBlockLootTableProvider {
+import de.wiederkehr.legomod.block.ModBlocks;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Set;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider {
+
+    public ModBlockLootTableProvider(HolderLookup.Provider registries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+    }
+
+    @Override
+    protected void generate() {
+
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    }
 }
