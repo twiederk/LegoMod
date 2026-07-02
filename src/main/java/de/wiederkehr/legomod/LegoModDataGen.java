d9f9152c22913bee2a519b02c16c0c8f54481f79
IN57KC <torsten.wiederkehr@ing.com>
2026-07-02 18:00:41 +0200

    Renamed LegoModDataGen

--- src/main/java/de/wiederkehr/legomod/LegoModDataGen.java
+++ src/main/java/de/wiederkehr/legomod/LegoModDataGen.java
@@ -9,7 +9,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @EventBusSubscriber(modid = LegoMod.MOD_ID)
-public class LegoModeDataGen {
+public class LegoModDataGen {
 
     @SubscribeEvent
     public static void gatherClientData(GatherDataEvent.Client event) {
