8dcec9f2116d7c196b14dfbea2362d12b8c207c1
IN57KC <torsten.wiederkehr@ing.com>
2026-07-06 09:28:53 +0200

    Start implemenation of metal detector

---
+++ src/main/java/de/wiederkehr/legomod/item/custom/MetalDetectorItem.java
@@ -0,0 +1,63 @@
+package de.wiederkehr.legomod.item.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class MetalDetectorItem extends Item {
+
+    public MetalDetectorItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        BlockPos positionClicked = context.getClickedPos();
+        Player player = context.getPlayer();
+
+        if (!level.isClientSide()) {
+            // We are on the server
+            boolean foundBlock = false;
+
+            for (int i = 0; i <= positionClicked.getY() + 64; i++) {
+                BlockState blockState = level.getBlockState(positionClicked.below(i));
+
+                if (isValuableBlock(blockState)) {
+                    outputValuableCoordinates(positionClicked.below(i), player, blockState.getBlock());
+                    foundBlock = true;
+
+                    // damage the item
+                    // play sound
+                    // spawn particles
+
+                    break;
+                }
+
+                if (!foundBlock) {
+                    outputNoValuablesFound(player);
+                }
+            }
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    private void outputNoValuablesFound(Player player) {
+
+    }
+
+    private void outputValuableCoordinates(BlockPos below, Player player, Block block) {
+
+    }
+
+    private boolean isValuableBlock(BlockState blockState) {
+        return blockState.is(Blocks.IRON_ORE) || blockState.is(Blocks.DEEPSLATE_IRON_ORE) ||
+                blockState.is(Blocks.DIAMOND_ORE) || blockState.is(Blocks.DEEPSLATE_DIAMOND_ORE);
+    }
+}
